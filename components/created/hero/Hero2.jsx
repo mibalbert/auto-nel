a681f62a65891e3b0771dde4a59b29f2c7fa253f
Alberto <mib.albert@gmail.com>
2023-09-21 03:30:31 +0300

    ADNASLDNALKSNDKLASNDKLANSDLNAKLDNLAKNLK

--- components/created/hero/Hero2.jsx
+++ components/created/hero/Hero2.jsx
@@ -23,87 +23,63 @@ const getModelsForMake = (make, data) => {
   const models = data
     .filter((el) => el.make === make)
     .map((el) => el.model);
-  return models;
+  return new Set(models);
 };
 
-const getProductionYears = (model, data) => {
-  const years = data.filter((el) => el.model === model).map((el) => el.productionYears);
-  return years;
+const getProductionYearsForModel = (model, data) => {
+  const years = data
+    .filter((el) => el.model === model)
+    .map((el) => el.productionYears);
+  return new Set(years);
 };
 
 const Hero2 = ({ carsData }) => {
-
   const router = useRouter();
-  // const searchParams = new URLSearchParams(router.query); // Use query from router
 
   const [make, setMake] = useState(baseCar.make);
   const [model, setModel] = useState(baseCar.model);
   const [years, setYears] = useState(baseCar.productionYears);
   const [availableModels, setAvailableModels] = useState([]);
-  const [productionYears, setProductionYears] = useState([]);
-
-  // useEffect(() => {
-  //   router.push(`/?make=${baseCar.make}&model=${baseCar.model}&years=${baseCar.productionYears}`);
-  // }, []);
-
-  const searchParams = useSearchParams()
+  const [availableYears, setAvailableYears] = useState([]);
 
   useEffect(() => {
-    // const makeParam = searchParams.get('make');
-    // const modelParam = searchParams.get('model');
-    // const yearsParam = searchParams.get('years');
+    setAvailableModels(Array.from(getModelsForMake(make, carsData)));
+  }, [make, carsData]);
 
-    // console.log(makeParam, modelParam, yearsParam);
-
-    // if (makeParam) {
-    //   setMake(makeParam);
-    // }
-
-    // if (modelParam) {
-    //   setModel(modelParam);
-    // }
-
-    // if (yearsParam) {
-    //   setYears(yearsParam);
-    // }
-
-    // const modelsForMake = getModelsForMake(makeParam, carsData);
-    // setAvailableModels(modelsForMake);
-    // const yearsForModel = getProductionYears(modelParam, carsData);
-    // setProductionYears(yearsForModel);
-
-  }, [searchParams, carsData]);
+  useEffect(() => {
+    if (model) {
+      setAvailableYears(Array.from(getProductionYearsForModel(model, carsData)));
+    } else {
+      setAvailableYears([]);
+    }
+  }, [model, carsData]);
 
   const handleMakeChange = (e) => {
     const newMake = e.target.value;
     setMake(newMake);
     setModel(""); // Reset model when changing make
-    setYears('');
-    router.replace(`/?make=${newMake}&model=undefined&years=undefined`);
-
-    const modelsForMake = getModelsForMake(newMake, carsData);
-    setAvailableModels(modelsForMake);
+    setYears(""); // Reset years when changing make
+    router.push(`/?make=${newMake}&model=undefined&years=undefined`);
   };
 
   const handleModelChange = (e) => {
     const newModel = e.target.value;
     setModel(newModel);
-    const yearsForModel = getProductionYears(newModel, carsData); // Corrected this line
-    setProductionYears(yearsForModel); // Corrected this line
-    router.replace(`/?make=${make}&model=${newModel}&years=${years}`);
+    setYears(""); // Reset years when changing model
+    router.push(`/?make=${make}&model=${newModel}&years=undefined`);
   };
 
   const handleYearsChange = (e) => {
     const newYears = e.target.value;
     setYears(newYears);
-    router.replace(`/?make=${make}&model=${model}&years=${newYears}`);
+    router.push(`/?make=${make}&model=${model}&years=${newYears}`);
   };
 
   const makes = Array.from(getMake(carsData));
 
   return (
     <section className="min-h-[90vh] bg-gradient-to-b from-transparent via-transparent to-[#ecedef] dark:to-[#1c1c1c] relative overflow-hidden">
-      <div className="absolute top-[30%] left-0 z-0 text-[300px] whitespace-nowrap font-bold font-serif bg-neutral-200 dark:bg-neutral-400 text-neutral-400 dark:text-neutral-200 border-y border-dashed">AUTO-NEL AUTO-NEL</div>
+      <div className="absolute top-[30%] left-0 z-0 text-[300px] whitespace-nowrap font-bold font-serif bg-neutral-200 dark:bg-neutral-700 text-neutral-400 dark:text-neutral-300 border-y border-dashed">AUTO-NEL AUTO-NEL</div>
       <div className="absolute top-0 left-0 z-50 w-full h-full"><div className="z-10 w-full h-full mx-auto border-dashed max-w-7xl border-x"></div> </div>
       <div className="absolute bottom-0 left-0 w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%] h-full z-50">
         <Image
@@ -122,7 +98,7 @@ const Hero2 = ({ carsData }) => {
             <form className="grid grid-cols-4 gap-5">
               <div className="flex flex-col col-span-1 gap-2">
                 <label className="text-lg font-semibold text-neutral-400">Make</label>
-                <select  value={make} onChange={handleMakeChange} className="w-full p-3 rounded-lg">
+                <select value={baseCar.make} onChange={handleMakeChange} className="w-full p-3 rounded-lg">
                   {makes.map((el, idx) => (
                     <option key={idx} value={el}>
                       {el}
@@ -133,8 +109,8 @@ const Hero2 = ({ carsData }) => {
 
               <div className="flex flex-col col-span-1 gap-2">
                 <label className="text-lg font-semibold text-neutral-400">Model</label>
-                <select value={model} onChange={handleModelChange} className="w-full p-3 rounded-lg">
-                  <option value="">Select a Model</option>
+                <select value={model} onChange={handleModelChange} className="w-full p-3 rounded-lg" disabled={!make}>
+                  <option value={baseCar.model}>{baseCar.model}</option>
                   {availableModels.map((el, idx) => (
                     <option key={idx} value={el}>
                       {el}
@@ -145,9 +121,9 @@ const Hero2 = ({ carsData }) => {
 
               <div className="flex flex-col col-span-1 gap-2">
                 <label className="text-lg font-semibold text-neutral-400">Years</label>
-                <select value={years} onChange={handleYearsChange} className="w-full p-3 rounded-lg">
-                  <option value="">Select a Year</option>
-                  {productionYears.map((el, idx) => (
+                <select value={years} onChange={handleYearsChange} className="w-full p-3 rounded-lg" disabled={!model}>
+                  <option value={baseCar.productionYears}>{baseCar.productionYears}</option>
+                  {availableYears.map((el, idx) => (
                     <option key={idx} value={el}>
                       {el}
                     </option>
@@ -162,4 +138,4 @@ const Hero2 = ({ carsData }) => {
   );
 };
 
-export default Hero2;
+export default Hero2; 
